5da97e708f2ebd243090534270844b94d4b33846
vijay.patel <to.patelvijay@gmail.com>
2019-02-19 09:12:35 +0530

    added comments

--- file_rename.sh
+++ file_rename.sh
@@ -2,6 +2,7 @@
 
 ###  THIS SCRIPT SEARCH ALL FILE EXTENSION WITH TXT AND WILL REPLACE NAME WITH SPACE
 
-search_files="*.txt"
+search_files="*.txt" # search file location of searching where you intent to replace filename
+
 find $1 -name "$search_files" -type f -print0 | \
   while read -d $'\0' f; do mv -v "$f" "${f// /_}"; done
