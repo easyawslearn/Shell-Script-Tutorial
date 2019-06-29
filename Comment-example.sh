4c7c67fa8fd90fe0143c74fb27a8e9cb57d8d45b
Vijay Patel <35554905+Patelvijaykumar@users.noreply.github.com>
2019-06-29 19:38:07 +0530

    Shell Script Comment Examples

---
+++ Comment-example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#single line comment example
+
+echo "single line comment example using # "
+
+echo "multiline comment comment exmple using"
+
+echo "multiline comment example <<COMMENT  COMMENT"
+<<COMMENT1
+
+Multiline comment example
+
+COMMENT1
